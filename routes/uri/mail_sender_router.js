d942f4725b4fe2b5b08cdfb0fba626685e1c0af2
Jose Ossorio <joseossorio99@hotmail.com>
2021-08-23 17:51:30 -0500

    change placeholder service return message

--- routes/uri/mail_sender_router.js
+++ routes/uri/mail_sender_router.js
@@ -6,7 +6,7 @@ const MailSenderService = require("../../services/service_mail_sender");
 router.route("/")
     .get((req, res) => {
         MailSenderService.checkEmailsToSend({isAutoSend: false})
-            .then(_ => res.send("lol"));
+            .then(_ => res.send("Sending emails"));
     });
 
 module.exports = router;
